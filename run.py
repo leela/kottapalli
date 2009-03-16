52c648b59069301ce763185ea20974af16904faa
leela <leela.vadlamud@gmail.com>
2009-03-16 17:56:18 +0530

    run.py compatible version for infogami

--- run.py
+++ run.py
@@ -1,36 +1,40 @@
-#! /usr/bin/env python
 """
 Sample run.py
 """
 import infogami
 
-try:
-    import settings
-except ImportError:
-    print 'no settings found'
-
 ## your db parameters
-infogami.config.db_parameters = dict(dbn='postgres', db="kottapalli2", user='anand', pw='')
+infogami.config.db_parameters = dict(dbn='postgres', db="kp_test", user='', pw='')
 
 ## site name 
-infogami.config.site = 'kottapalli.in'
+infogami.config.site = 'infogami.org'
 infogami.config.admin_password = "admin123"
 
 ## add additional plugins and plugin path
 infogami.config.plugin_path += ['plugins']
-infogami.config.plugins += ['kottapalli']
+infogami.config.plugins += ['kottapalli', 'api']
 
 def createsite():
     import web
-    from infogami.infobase.infobase import Infobase
+    from infogami.infobase import dbstore, infobase, config, server
     web.config.db_parameters = infogami.config.db_parameters
     web.config.db_printing = True
-    web.load()
-    Infobase().create_site(infogami.config.site, infogami.config.admin_password)
+    web.ctx.ip = '127.0.0.1'
+
+    server.app.request('/')
+    schema = dbstore.Schema()
+    store = dbstore.DBStore(schema)
+    ib = infobase.Infobase(store, config.secret_key)
+    ib.create(infogami.config.site)
 
 if __name__ == "__main__":
     import sys
-    if '--createsite' in sys.argv:
+
+    if '--schema' in sys.argv:
+        from infogami.infobase.dbstore import Schema
+        print Schema().sql()
+    elif '--createsite' in sys.argv:
         createsite()
     else:
         infogami.run()
+
